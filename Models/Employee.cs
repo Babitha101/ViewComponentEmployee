2a8f4fac44218f7cd55555ba6b9769b3c648428e
Babitha105 <babitha.murugesan@gmail.com>
2021-11-25 19:03:25 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponentEmployee.Models
+{
+    public class Employee
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponentEmployee.Models
+{
+    public interface IEmployeeRepository
+    {
+
+        IEnumerable<Employee> GetEmployees();
+        Employee GetEmployee(int id);
+        Employee Update(Employee updatedemployee);
+        Employee Add(Employee employee);
+        Employee Delete(int id);
+
+        //IEnumerable<DeptHeadCount> EmployeeCountByDept();
+    }
+}
